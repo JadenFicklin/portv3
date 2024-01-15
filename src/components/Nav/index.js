9f3afd0f3b7b8c36f0889811dc02e068eafd8cf6
JadenFicklin <jadendficklin@gmail.com>
2024-01-15 12:36:38 -0700

    nav ready, just need to hook element positions to active link state

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import c from 'classnames';
 
 function Index() {
-  const [activeLink, setActiveLink] = useState(null);
+  const [activeLink, setActiveLink] = useState('ABOUT');
 
   const navOptions = [
     {
@@ -34,17 +34,17 @@ function Index() {
         <div className="text-xs">
           <Link
             onClick={() => setActiveLink(item.name)}
-            className="flex items-center py-3">
+            className="flex items-center py-3 group">
             <div
               className={c(
-                'h-[1px] duration-300 mr-4',
+                'h-[1px] duration-300 mr-4 group-hover:w-[60px] group-hover:bg-light',
                 activeLink === `${item.name}`
                   ? 'w-[60px] bg-light'
                   : 'w-[30px] bg-lightDarker'
               )}></div>
             <p
               className={c(
-                'font-semibold tracking-[1px]',
+                'font-semibold tracking-[1px] group-hover:text-medium',
                 activeLink === `${item.name}`
                   ? 'text-medium'
                   : 'text-lightDarker'
