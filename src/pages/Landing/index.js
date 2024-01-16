5cebff38b9a19248c715faed063fbb44e2fd5ea3
JadenFicklin <jadendficklin@gmail.com>
2024-01-16 12:45:07 -0700

    removes the useEffect scroll

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -10,33 +10,19 @@ import Icons from '../../components/Icons';
 import { cn } from '../../utils/cn';
 
 function Index() {
-  const rightSectionRef = useRef(null);
-
-  useEffect(() => {
-    const handleScroll = (event) => {
-      // Check if the cursor is over the right section
-      if (
-        rightSectionRef.current &&
-        !rightSectionRef.current.contains(event.target)
-      ) {
-        rightSectionRef.current.scrollBy(0, event.deltaY);
-      }
-    };
-
-    window.addEventListener('wheel', handleScroll, { passive: true });
-
-    return () => window.removeEventListener('wheel', handleScroll);
-  }, []);
-
-  const padding = 'px-6 py-12 md:px-12 md:py-20 lg:px-24';
+  const padding = 'px-6 py-12 md:px-12 md:py-20 lg:px-24 lg:py-24';
 
   return (
     <>
-      <div className="w-full h-screen bg-background">
-        <div className="h-screen max-w-screen-xl mx-auto lg:grid lg:gap-4 lg:grid-cols-2">
+      <div className="w-full bg-background">
+        <div className="max-w-screen-xl mx-auto lg:grid lg:gap-4 lg:grid-cols-2">
           {/* Left section */}
+          <div className="h-screen w-[500px] hidden lg:block" />
           <div
-            className={cn('flex flex-col justify-between lg:py-24', padding)}>
+            className={cn(
+              'flex flex-col justify-between h-screen lg:fixed w-full lg:w-[500px]',
+              padding
+            )}>
             <div>
               <Hero />
               <Nav />
@@ -44,12 +30,7 @@ function Index() {
             <Icons />
           </div>
           {/* Right section */}
-          <div
-            ref={rightSectionRef}
-            className={cn(
-              'h-screen lg:overflow-auto lg:py-24 hide-scrollbar',
-              padding
-            )}>
+          <div className={cn(padding)}>
             <About />
             <Experience />
             <Work />
